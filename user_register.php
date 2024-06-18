b7533b1b8a8719ba8fbee6031157cfe407770e87
NaufalAnanta <115007513+NaufalAnantaSE@users.noreply.github.com>
2024-06-18 20:51:21 +0700

    Update user_register.php

--- user_register.php
+++ user_register.php
@@ -1,44 +1,37 @@
 <?php
-
 include 'components/connect.php';
 
 session_start();
 
-if(isset($_SESSION['user_id'])){
-   $user_id = $_SESSION['user_id'];
-}else{
-   $user_id = '';
-};
-
-if(isset($_POST['submit'])){
-
-   $name = $_POST['name'];
-   $name = filter_var($name, FILTER_SANITIZE_STRING);
-   $email = $_POST['email'];
-   $email = filter_var($email, FILTER_SANITIZE_STRING);
-   $pass = sha1($_POST['pass']);
-   $pass = filter_var($pass, FILTER_SANITIZE_STRING);
-   $cpass = sha1($_POST['cpass']);
-   $cpass = filter_var($cpass, FILTER_SANITIZE_STRING);
-
-   $select_user = $conn->prepare("SELECT * FROM `users` WHERE email = ?");
-   $select_user->execute([$email,]);
-   $row = $select_user->fetch(PDO::FETCH_ASSOC);
-
-   if($select_user->rowCount() > 0){
-      $message[] = 'email sudah terdaftar';
-   }else{
-      if($pass != $cpass){
-         $message[] = 'komfirmasi Password Tidak Sama';
-      }else{
-         $insert_user = $conn->prepare("INSERT INTO `users`(name, email, password) VALUES(?,?,?)");
-         $insert_user->execute([$name, $email, $cpass]);
-         $message[] = 'Pendaftaran Berhasil, Silahkan Login';
-      }
-   }
-
+if (isset($_SESSION['user_id'])) {
+    $user_id = $_SESSION['user_id'];
+} else {
+    $user_id = '';
 }
 
+if (isset($_POST['submit'])) {
+    $name = filter_var($_POST['name'], FILTER_SANITIZE_STRING);
+    $email = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL); // Gunakan FILTER_SANITIZE_EMAIL untuk sanitasi email
+    $pass = sha1($_POST['pass']); // Secara umum, sebaiknya hindari penggunaan SHA1 untuk password, gunakan hashing yang lebih kuat seperti bcrypt
+    $cpass = sha1($_POST['cpass']); // Gunakan FILTER_SANITIZE_STRING untuk sanitasi string
+
+    $select_user = $conn->prepare("SELECT * FROM `users` WHERE email = ?");
+    $select_user->execute([$email]);
+    $row = $select_user->fetch(PDO::FETCH_ASSOC);
+
+    if ($select_user->rowCount() > 0) {
+        $message[] = 'Email sudah terdaftar';
+    } else {
+        if ($pass != $cpass) {
+            $message[] = 'Konfirmasi Password Tidak Sama';
+        } else {
+            // Disarankan untuk menggunakan prepared statement untuk mencegah SQL injection
+            $insert_user = $conn->prepare("INSERT INTO `users`(name, email, password) VALUES(?,?,?)");
+            $insert_user->execute([$name, $email, $cpass]);
+            $message[] = 'Pendaftaran Berhasil, Silahkan Login';
+        }
+    }
+}
 ?>
 
 <!DOCTYPE html>
@@ -49,45 +42,30 @@ if(isset($_POST['submit'])){
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
    <title>Daftar</title>
    
-   <link rel="icon" type="image/jpg" href="images\favicon.jpg">
+   <link rel="icon" type="image/jpg" href="images/favicon.jpg">
 
    <!-- font awesome cdn link  -->
    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
 
    <!-- custom css file link  -->
    <link rel="stylesheet" href="css/style.css">
-
 </head>
 <body>
    
 <?php include 'components/user_header.php'; ?>
 
 <section class="form-container">
-
    <form action="" method="post">
       <h3>Buat Akun Baru</h3>
-      <input type="text" name="name" required placeholder="enter your username" maxlength="20"  class="box">
-      <input type="email" name="email" required placeholder="enter your email" maxlength="50"  class="box" oninput="this.value = this.value.replace(/\s/g, '')">
-      <input type="password" name="pass" required placeholder="enter your password" maxlength="20"  class="box" oninput="this.value = this.value.replace(/\s/g, '')">
-      <input type="password" name="cpass" required placeholder="confirm your password" maxlength="20"  class="box" oninput="this.value = this.value.replace(/\s/g, '')">
-      <input type="submit" value="register now" class="btn" name="submit">
+      <input type="text" name="name" required placeholder="Masukkan username Anda" maxlength="20" class="box">
+      <input type="email" name="email" required placeholder="Masukkan email Anda" maxlength="50" class="box" oninput="this.value = this.value.replace(/\s/g, '')">
+      <input type="password" name="pass" required placeholder="Masukkan password Anda" maxlength="20" class="box" oninput="this.value = this.value.replace(/\s/g, '')">
+      <input type="password" name="cpass" required placeholder="Konfirmasi password Anda" maxlength="20" class="box" oninput="this.value = this.value.replace(/\s/g, '')">
+      <input type="submit" value="Daftar Sekarang" class="btn" name="submit">
       <p>Sudah punya Akun?</p>
       <a href="user_login.php" class="option-btn">Login Sekarang</a>
    </form>
-
 </section>
-
-
-
-
-
-
-
-
-
-
-
-
 
 <?php include 'components/footer.php'; ?>
 
