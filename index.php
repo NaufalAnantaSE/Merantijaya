aaf16651dee563a040d56161461705b51b9cbeae
NaufalAnanta <115007513+NaufalAnantaSE@users.noreply.github.com>
2024-06-19 00:14:09 +0700

    Update index.php

---
+++ index.php
@@ -0,0 +1,5 @@
+// 1. Naufal ananta 2211104081
+// 2. Danuar ihza mahendra 2211104084
+// 3. Putra Pratama Okta Riano 2211104068
+// 4. allaya dafa zhillal 2211104090
+// 5. Naufal Maulana Izhudin2211104091
