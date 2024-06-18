4f72ba3eac4bf7b498e29700688613682f096c19
NaufalAnanta <115007513+NaufalAnantaSE@users.noreply.github.com>
2024-06-18 21:12:10 +0700

    Update user_login.php

--- user_login.php
+++ user_login.php
@@ -14,9 +14,7 @@ if (isset($_POST['submit'])) {
 
    if (isset($_POST['name']) && isset($_POST['pass'])) {
       $name = $_POST['name'];
-      $name = filter_var($name, FILTER_SANITIZE_STRING);
-      $pass = sha1($_POST['pass']);
-      $pass = filter_var($pass, FILTER_SANITIZE_STRING);
+      $pass = sha1($_POST['pass']); // Gunakan hashing yang lebih kuat seperti bcrypt
 
       // Cek pengguna di tabel users
       $select_user = $conn->prepare("SELECT * FROM `users` WHERE name = ? AND password = ?");
@@ -29,10 +27,6 @@ if (isset($_POST['submit'])) {
          header('location:page.php?mod=home');
          exit;
       } else  {
-         $name = $_POST['name'];
-         $name = filter_var($name, FILTER_SANITIZE_STRING);
-         $pass = sha1($_POST['pass']);
-         $pass = filter_var($pass, FILTER_SANITIZE_STRING);
          // Cek admin di tabel admins
          $select_admin = $conn->prepare("SELECT * FROM `admins` WHERE name = ? AND password = ?");
          $select_admin->execute([$name, $pass]);
@@ -43,11 +37,11 @@ if (isset($_POST['submit'])) {
             header('location:?mod=homeadmin');
             exit;
          } else {
-            $message[] = 'username atau password salah';
+            $message[] = 'Username atau password salah';
          }
       }
    } else {
-      $message[] = 'mohon lengkapi data';
+      $message[] = 'Mohon lengkapi data';
    }
 }
 
@@ -62,7 +56,7 @@ if (isset($_POST['submit'])) {
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
    <title>Login</title>
 
-   <link rel="icon" type="image/jpg" href="images\favicon.jpg">
+   <link rel="icon" type="image/jpg" href="images/favicon.jpg">
 
    <!-- font awesome cdn link  -->
    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
@@ -80,11 +74,9 @@ if (isset($_POST['submit'])) {
 
       <form action="" method="post">
          <h3>Login Sekarang</h3>
-         <input type="text" name="name" required placeholder="enter your name" maxlength="50" class="box"
-            oninput="this.value = this.value.replace(/\s/g, '')">
-         <input type="password" name="pass" required placeholder="enter your password" maxlength="20" class="box"
-            oninput="this.value = this.value.replace(/\s/g, '')">
-         <input type="submit" value="login now" class="btn" name="submit">
+         <input type="text" name="name" required placeholder="Masukkan nama Anda" maxlength="50" class="box">
+         <input type="password" name="pass" required placeholder="Masukkan password Anda" maxlength="20" class="box">
+         <input type="submit" value="Login Sekarang" class="btn" name="submit">
          <p>Belum Punya Akun?</p>
          <a href="user_register.php" class="option-btn">Daftar Sekarang</a>
       </form>
